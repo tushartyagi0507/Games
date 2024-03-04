e89df27ab2279da984b4dbeef52d0c73400d8a65
Tushar Tyagi <tushartyagi0507@gmail.com>
2024-03-04 16:07:25 +0530

    Prep for FoodApp

--- FoodApp/script.js
+++ FoodApp/script.js
@@ -1,1 +1,9 @@
-console.log("hello");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.querySelector(".root"));
+const heading = (
+    <h1>Hello</h1>
+)
+
+root.render(heading);
